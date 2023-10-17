a0fb048d6ec7f9c50e6a1c7a4be7bdfc98b9d9df
GHM-0 <gustavohmarins@gmail.com>
2023-10-17 00:30:31 -0300

    PlayStore

---
+++ POKEDEX_ANGULAR/src/app/services/pokemon-api.service.ts
@@ -0,0 +1,24 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import { Observable } from 'rxjs';
+import {environment} from'src/environments/environment';
+import {PokemonData} from 'src/app/models/pokemonData'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonAPIService {
+ private baseURL:string='';
+	private pokeData:PokemonData|any;
+
+  constructor(private http:HttpClient) {
+		this.baseURL=environment.pokeAPI;
+	 }
+
+
+	getPokemon(name:string):Observable<PokemonData>{
+
+		this.pokeData=this.http.get<PokemonData>(`${this.baseURL}${name}`);
+		return this.pokeData;
+	}
+}
